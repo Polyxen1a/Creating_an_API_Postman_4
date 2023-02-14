078f314365d9531ee8fd0b354252663c1ba2bbaa
Polyxen1a <maslowa@inbox.ru>
2023-02-14 14:02:08 +0300

    Initial commit

---
+++ src/main/java/ru/skypro/creating_an_api_postman_4/CreatingAnApiPostman4Application.java
@@ -0,0 +1,13 @@
+package ru.skypro.creating_an_api_postman_4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreatingAnApiPostman4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreatingAnApiPostman4Application.class, args);
+	}
+
+}
---
+++ src/test/java/ru/skypro/creating_an_api_postman_4/CreatingAnApiPostman4ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.creating_an_api_postman_4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreatingAnApiPostman4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
